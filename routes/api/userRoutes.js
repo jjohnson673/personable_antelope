84b7d5c84b454c7b349c297ab2664b4541ff1074
jjohnson673 <jkjohnson673@gmail.com>
2023-01-28 18:55:24 -0500

    added in userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,13 +1,36 @@
-// /api/users GET all and POST
+const router = require('express').Router();
 
+const {
+    getUser,
+    getIndvUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+
+
+// /api/users GET all and POST
+router.route('/').get(getUser).post(createUser);
 
 
 
 
 // /api/users/:userId GET one user, PUT and DELETE by user's ID
-
+router.route('/:userId')
+.get(getIndvUser)
+.put(updateUser)
+.delete(deleteUser);
 
 
 
 
 // /api/users/:userId/friends/:friendId POST and DELETE a friend by ID
+
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
