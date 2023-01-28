c902e99abb65da905ce2940554eff24ad8bbcd8f
jjohnson673 <jkjohnson673@gmail.com>
2023-01-28 18:57:22 -0500

    added in frame for thoughtRoutes.js

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,19 @@
+// /api/thoughts GET all and POST thought
+
+
+
+
+
+// /api/thoughts/:thoughtId GET single thought, PUT and DELETE by iD
+
+
+
+
+//  /api/thoughts/:thoughtId/reactions POST new reactions
+
+
+
+
+// /api/thoughts/:thoughtId/reactions/:reactionId DELETE reaction by ID
+
+
