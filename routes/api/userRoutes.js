14d8373e948abc6ce398ec48535bb60d641aa3a7
jjohnson673 <jkjohnson673@gmail.com>
2023-01-28 18:51:21 -0500

    added in api routes and basic outline of what needs to be done

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+// /api/users GET all and POST
+
+
+
+
+
+// /api/users/:userId GET one user, PUT and DELETE by user's ID
+
+
+
+
+
+// /api/users/:userId/friends/:friendId POST and DELETE a friend by ID
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+// Import all of the API routes from /api/index.js 
+
+
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
