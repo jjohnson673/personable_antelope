6ecc8df3b8daab8d1efbad78963e64c170ebad86
jjohnson673 <jkjohnson673@gmail.com>
2023-01-28 19:45:04 -0500

    add/delete friend and delete user with associated thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,15 +45,50 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  //delete user
-
 
   // add friend
-
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userID },
+      { $addToSet: { friends: req.params.friendID } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found associated with this ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
   // delete friend
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userID },
+      { $pull: { friends: req.params.friendID } },
+      { new: true }
+    )
+      .then(
+        (user) =>
+          !user
+            ? res.status(404).json({ message: "No user found associated with this ID" })
+            : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
 
 
+  //Delete user with extra credit: remove associated thoughts when user is deleted
 
-  //extra credit: remove associated thoughts when user is deleted
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userID })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found associated with this ID" })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: "User and their associated thoughts have been deleted" }))
+      .catch((err) => res.status(500).json(err));
+  },
+};
