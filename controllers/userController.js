d95a964be04688ef1807015331294bb960ff4d98
jjohnson673 <jkjohnson673@gmail.com>
2023-01-28 19:40:04 -0500

    GET all/indv users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,10 +2,25 @@ const { User, Thought } = require("../models");
 
 module.exports = {
   //Get all users
-
+  getUser(req, res) {
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
 
   //get individual user
-
+  getIndvUser(req, res) {
+    User.findOne({ _id: req.params.userID })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found associated with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
   //create user 
   
 
@@ -21,5 +36,5 @@ module.exports = {
 
 
 
-  
+
   //extra credit: remove associated thoughts when user is deleted
